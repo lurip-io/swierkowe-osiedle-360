95bfc4e3953724a6b52113b747cb511a8bb52edf
Michał Kozioł <mkoziol@unity-t.pl>
2024-02-29 15:03:39 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,461 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-wejcie",
+      "name": "Wejście",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3635993899637084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6349354421540632,
+          "pitch": 0.34431570222399266,
+          "rotation": 0,
+          "target": "1-korytarz---wieszak"
+        },
+        {
+          "yaw": 1.6138732443253048,
+          "pitch": 0.17851727263334993,
+          "rotation": 0,
+          "target": "2-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-korytarz---wieszak",
+      "name": "Korytarz - Wieszak",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07016348135941897,
+          "pitch": 0.6845204361161557,
+          "rotation": 0,
+          "target": "2-korytarz"
+        },
+        {
+          "yaw": 0.02336992680213612,
+          "pitch": 0.13548341163674316,
+          "rotation": 0,
+          "target": "7-sypialnia-gwna"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-korytarz",
+      "name": "Korytarz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1799449393072532,
+          "pitch": 0.16590579663324512,
+          "rotation": 0,
+          "target": "0-wejcie"
+        },
+        {
+          "yaw": 0.0714062225605776,
+          "pitch": 0.7042878801677066,
+          "rotation": 0,
+          "target": "1-korytarz---wieszak"
+        },
+        {
+          "yaw": 1.4973253055894125,
+          "pitch": 0.3602006988200088,
+          "rotation": 0,
+          "target": "4-biuro"
+        },
+        {
+          "yaw": 1.7727070824745779,
+          "pitch": 0.1672758224168973,
+          "rotation": 0,
+          "target": "5-sypialnia-2"
+        },
+        {
+          "yaw": -2.43174614094975,
+          "pitch": 0.2732316621865891,
+          "rotation": 0,
+          "target": "3-azienka"
+        },
+        {
+          "yaw": -2.921622663938292,
+          "pitch": 0.19858145114978498,
+          "rotation": 0,
+          "target": "7-sypialnia-gwna"
+        },
+        {
+          "yaw": 2.9982647035162406,
+          "pitch": 0.3634523666441165,
+          "rotation": 0,
+          "target": "6-salon--aneks"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.150798775968724,
+          "pitch": 0.3064495337646562,
+          "rotation": 0,
+          "target": "6-salon--aneks"
+        },
+        {
+          "yaw": -2.02936791514335,
+          "pitch": 0.19527907569923286,
+          "rotation": 0,
+          "target": "4-biuro"
+        },
+        {
+          "yaw": -1.9268815851335823,
+          "pitch": 0.022961157592176207,
+          "rotation": 0,
+          "target": "5-sypialnia-2"
+        },
+        {
+          "yaw": -1.9645060085979509,
+          "pitch": 0.644904102161755,
+          "rotation": 0,
+          "target": "2-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-biuro",
+      "name": "Biuro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.406532791304585,
+          "pitch": 0.29360777410044747,
+          "rotation": 0,
+          "target": "2-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sypialnia-2",
+      "name": "Sypialnia 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4884102188231036,
+          "pitch": 0.2133578205034059,
+          "rotation": 0,
+          "target": "2-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-salon--aneks",
+      "name": "Salon + Aneks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0359686763147113,
+          "pitch": 0.129317925879338,
+          "rotation": 0,
+          "target": "3-azienka"
+        },
+        {
+          "yaw": 1.824507846497597,
+          "pitch": 0.16301648164278149,
+          "rotation": 0,
+          "target": "7-sypialnia-gwna"
+        },
+        {
+          "yaw": 0.038621324676729074,
+          "pitch": 0.14279737089957933,
+          "rotation": 0,
+          "target": "8-taras"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sypialnia-gwna",
+      "name": "Sypialnia Główna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.010775752543207062,
+          "pitch": 0.12961063393197847,
+          "rotation": 0,
+          "target": "1-korytarz---wieszak"
+        },
+        {
+          "yaw": 3.1133703670996535,
+          "pitch": 0.2489951242645425,
+          "rotation": 0,
+          "target": "8-taras"
+        },
+        {
+          "yaw": 0.2704238489721522,
+          "pitch": 0.2664303294298449,
+          "rotation": 0,
+          "target": "6-salon--aneks"
+        },
+        {
+          "yaw": -0.1319852825152914,
+          "pitch": 0.24140220341202578,
+          "rotation": 0,
+          "target": "3-azienka"
+        },
+        {
+          "yaw": 0.07282206085763931,
+          "pitch": 0.343281676995856,
+          "rotation": 0,
+          "target": "2-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-taras",
+      "name": "Taras",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8207696966437767,
+          "pitch": 0.25387544842636167,
+          "rotation": 0,
+          "target": "6-salon--aneks"
+        },
+        {
+          "yaw": 0.7604632687596933,
+          "pitch": 0.1345768167779191,
+          "rotation": 0,
+          "target": "7-sypialnia-gwna"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Świerkowe Osiedle (Parter)",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
